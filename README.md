4a728f20c1ef3c2af623d18e05d41f8f0db31667
cTahirih <tahysluana@gmail.com>
2018-09-14 02:24:39 -0500

    feat(project) init project with layout

---
+++ README.md
@@ -0,0 +1,26 @@
+# ladrillos_lark
+
+> Landing Page Ladrillos Lark 
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+## Team
+* Jhoel Paico - Designer
+* Tahirih Jaliri - Front-end developer
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  #App
+    // - landing page Ladrillos Lark cintiatahirih.jp@gmail.com
+    header
+      .header__logo
+        img(src="", alt="Ladrillos Lark")
+      nav.header__navbar
+        ul
+          li PRODUCTOS
+          li CLIENTES
+          li UBÍCANOS
+          li COTIZAR
+          li NOTICIAS
+          li PROCESO
+    main
+      section
+        h1    
+          | ¿CÓMO PRODUCIR LADRILLOS DE CALIDAD?
+        p
+          | La producción remota desde tiempos antiguos y el uso empleado ha evolucionado en conjunto con los nuevos métodos de fabricación que han surgido.
+        p
+          | Hoy en día es más práctico poder fabricarlos. No existe complejidad en convertir la arcilla, el agua y la tierra en conjunto en un ladrillo. A continuación, verás las etapas que siguen los fabricantes hasta llegar al resultado final. Use el scroll(rueda del mouse) para navegar.
+        ul
+          li 
+            span  01.-
+            | Extracción
+          li 
+            span  02.-
+            | Molienda
+          li
+            span  03.-
+            | Prensadora
+          li
+            span  04.-
+            | Cortadora
+          li
+            span  05.-
+            | Secadora
+          li
+            span  06.-
+            | Apiladero
+          li
+            span  07.-
+            | Prehorno
+          li
+            span  08.-
+            | Horno
+      section
+        h2  Bienvenido
+        p
+          | Ahora conocerás el proceso productivo para la correcta realización de unos ladrillos de calidad
+      section
+        div
+          span Primer nivel
+          h3  Extracción
+          h4  Materia Prima
+          
+    footer
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+}
+</script>
+
+<style>
+
+</style>
